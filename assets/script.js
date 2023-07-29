8c93b638f5ee5675a41ff37e852f853e9125cfcc
ethan <hertlethan@gmail.com>
2023-07-29 17:56:46 -0400

    added js code to hide start menu when start button is clicked and to begin timer countdown

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,26 @@ Created 2023/07/29
 Last Edited 2023/07/29
 */
 
+var timeLeft = 60;
+var quizTimer = document.getElementById("timer");
+
+//begins quiz attempt when start button is clicked
+document.getElementById("start-button").addEventListener("click", function beginQuizAttempt()
+{
+    //hides start menu
+    document.getElementById("start-menu").setAttribute("style", "display: none");
+
+    //begins quiz timer countdown as questions start to appear
+    var quizTimerCountdown = setInterval(function()
+    {
+        timeLeft--;
+        quizTimer.textContent = "Timer: " + timeLeft;
+
+        if(timeLeft <= 0)
+        {
+            clearInterval(quizTimerCountdown);
+            return;
+        }
+    }, 1000);
+});
+
