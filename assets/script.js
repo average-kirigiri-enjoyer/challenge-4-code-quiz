1b29461532a013ffb0db87e0e37f9d63f3714302
ethan <hertlethan@gmail.com>
2023-07-29 18:55:01 -0400

    added basic html structure and css styles for quiz questions

--- assets/script.js
+++ assets/script.js
@@ -8,13 +8,38 @@ Last Edited 2023/07/29
 var timeLeft = 60;
 var quizTimer = document.getElementById("timer");
 
+function generateQuestion1()
+{
+
+}
+
+function generateQuestion2()
+{
+    
+}
+
+function generateQuestion3()
+{
+    
+}
+
+function generateQuestion4()
+{
+    
+}
+
+function generateQuestion5()
+{
+    
+}
+
 //begins quiz attempt when start button is clicked
 document.getElementById("start-button").addEventListener("click", function beginQuizAttempt()
 {
     //hides start menu
     document.getElementById("start-menu").setAttribute("style", "display: none");
 
-    //begins quiz timer countdown as questions start to appear
+    //begins quiz timer countdown, quiz ends once timer is less than or equal to 0
     var quizTimerCountdown = setInterval(function()
     {
         timeLeft--;
@@ -23,7 +48,6 @@ document.getElementById("start-button").addEventListener("click", function begin
         if(timeLeft <= 0)
         {
             clearInterval(quizTimerCountdown);
-            return;
         }
     }, 1000);
 });
